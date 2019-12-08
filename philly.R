383370d47a941de2a06ff9488756cd69da9da527
James Wade <github@jameshwade.com>
2019-12-08 15:38:54 -0500

    update plots

--- philly.R
+++ philly.R
@@ -2,15 +2,12 @@ library(tidyverse)
 library(leaflet)
 library(ggthemes)
 library(gganimate)
-# library(cowplot)
-# library(patchwork)
-# library(ggtext)
-# library(osmdata)
-# library(sf)
-# library(ggpointdensity)
-# library(beepr)
+library(ggpubr)
+library(osmdata)
+library(sf)
+library(ggpointdensity)
 
-
+# Load in data set
 if (!file.exists('data/2019-49_tickets.csv')) {
   d = read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-12-03/tickets.csv')
   write_csv(d, 'data/2019-49_tickets.csv')
@@ -18,46 +15,62 @@ if (!file.exists('data/2019-49_tickets.csv')) {
 
 d <- read_csv('data/2019-49_tickets.csv')
 
-philly <- us.cities %>% filter(name == "Philadelphia PA")
+# Collect map data for Philadelphia
+coord = getbb('Philadelphia, PA')
 
-glimpse(d)
+streets1 = opq(coord) %>% 
+  add_osm_feature('highway', c('motorway', 'primary', 'secondary', 'tertiary')) %>% 
+  osmdata_sf()
 
-d2 <- d %>% filter(lat > 39.8 & lon > -75.4) %>% 
+streets2 = opq(coord) %>% 
+  add_osm_feature('highway', c('residential', 'living_street', 'unclassified', 'service', 'foodway')) %>% 
+  osmdata_sf()
+
+# Clean data, mostly cleaning up violation names and separating date info
+d2 <- d %>% filter(between(lon, coord[1, 1], coord[1, 2]) & 
+                     between(lat, coord[2, 1], coord[2, 2])) %>% 
   mutate(year = lubridate::year(issue_datetime),
          month = lubridate::month(issue_datetime),
          hour = lubridate::hour(issue_datetime),
          day = lubridate::day(issue_datetime),
          wday = lubridate::wday(issue_datetime),
          violation_desc = str_squish(str_remove(pattern = "CC|PING", violation_desc)),
-         type = fct_lump(violation_desc, n = 5, other_level = "OTHER"))
+         type = fct_lump(violation_desc, n = 5, other_level = "OTHER"),
+         type_2 = as.numeric(type))
 
-ggplot() + geom_polygon(data = usa, aes(x=long, y = lat, group = group)) + 
-  coord_fixed(1.3)
+t1 <- d2 %>% 
+  select(type_2, type) %>% 
+  distinct() %>% 
+  arrange(type_2) %>% 
+  rename(Index = type_2, Violation = type) %>% 
+  ggtexttable(rows = NULL, theme = ttheme(tbody.style = tbody_style(fill = alpha('white', 0.5)),
+                                          colnames.style = colnames_style(fill = alpha('white', 0.5)))) %>% 
+  ggplotGrob()
 
-ggplot() + geom_polygon(data = sample_n(d2, 100), aes(x=lon, y = lat, group = type)) + 
-  coord_fixed(1.3)
+p <- ggplot() + 
+  geom_sf(data = streets2$osm_lines, col = 'grey20', size = .1) +
+  geom_sf(data = streets1$osm_lines, col = 'grey20', size = .1) +
+  geom_pointdensity(data = sample_n(d2, 1e4), aes(y = lat, x = lon), alpha = 0.2) +
+  coord_sf(xlim = coord[1,], ylim = coord[2,], expand = FALSE) +
+  theme_void() +
+  scale_color_viridis_c(option = 'viridis') + labs(color = "Ticket Density") +
+  theme(legend.background = element_rect(fill=alpha('white', 0.5)),
+        legend.position = c(0.8, 0.2)) +
+  ggtitle("Philadelphia Parking Tickets - 2017 \nAll Ticket Types")
 
-ggplot(sample_n(d2, 1e3)) + 
-  # geom_point(aes(x = lat, y = lon, color = type), alpha = 0.2) +
-  theme_void() + 
-  scale_color_brewer(palette = "Spectral") +
-  labs(color = "VIOLATION") +
-  guides(colour = guide_legend(override.aes = list(alpha = 1))) +
-  geom_polygon(aes(x = lat, y = lon)) +
-  coord_fixed(1.3)
+ggsave(filename = "philly_tickets.png", plot = p)
 
-ggplot(sample_n(d2, 1e4)) + 
-  ggpointdensity::geom_pointdensity(aes(x = lat, y = lon)) +
-  theme_void() + 
-  scale_color_viridis_c(option = 'viridis') +
-  transition_states(
-    type,
-    transition_length = 2,
-    state_length = 2
-  ) +
-  labs(title = 'Violation: {closest_state}', color = "")
+p_anim <- p + transition_states(type_2, transition_length = 1, state_length = 2) +
+  ggtitle('Philadelphia Parking Tickets - 2017 \nViolation Type: {closest_state}') +
+  enter_fade() + 
+  exit_shrink() +
+  ease_aes('sine-in-out') +
+  annotation_custom(grob = t1, ymax = 39.975, xmin = -75.13) +
+  theme(legend.position = c(0.88, 0.88))
 
-ggplot(sample_n(d2, 1e4)) + 
+anim_save(filename = "philly_tickets_animated.gif", animation = p_anim)
+
+p_byhour <- ggplot(sample_n(d2, 1e4)) + 
   geom_bar(aes(fill = type, x = hour)) + 
   coord_polar(start = -.14) +
   scale_fill_brewer(palette = "Spectral") +
@@ -72,31 +85,33 @@ ggplot(sample_n(d2, 1e4)) +
         panel.background = element_blank(),
         plot.title = element_text(face = "bold", size = 14))
 
+ggsave(filename = "philly_tickets_byhour.png", plot = p_byhour)
+
 n_days <- d2 %>% select(month, day, year) %>% distinct() %>% nrow()
 
-ggplot(d2) + 
+p_bytype <- ggplot(d2) + 
   geom_bar(aes(x = type, fill = type), width = 0.3) +
   scale_fill_brewer(palette = "Spectral") +
-  scale_y_continuous(labels = function(x) x / n_days, breaks = c(0, 250, 500, 750, 1000, 1250) * n_days) +
+  scale_y_continuous(labels = function(x) x / n_days, 
+                     breaks = c(0, 250, 500, 750, 1000, 1250) * n_days) +
   labs(y = "Tickets per Day", x = "Violation") +
   coord_flip() +
   theme_classic() +
   theme(legend.position = "none")
 
-
-factpal <- colorFactor(topo.colors(6), d2$type)
-m <- leaflet(sample_n(d2, 1e4)) %>% 
-  setView(lng = median(d2$lon), lat = median(d2$lat), zoom = 12) %>%
-  addCircles(lng = ~ lon, lat = ~ lat, color = ~ factpal(type)) %>% 
-  addTiles()
-
-m %>% addProviderTiles(providers$CartoDB.Positron)
-
-m %>% addTiles()
+ggsave(filename = "philly_tickets_byday.png", plot = p_bytype)
 
 
-m <- leaflet() %>%
-  addTiles() %>%  # Add default OpenStreetMap map tiles
-  addMarkers(lng=174.768, lat=-36.852, popup="The birthplace of R") %>% 
-  
-m
+# factpal <- colorFactor(topo.colors(6), d2$type)
+# m <- leaflet(sample_n(d2, 1e4)) %>% 
+#   setView(lng = median(d2$lon), lat = median(d2$lat), zoom = 12) %>%
+#   addCircles(lng = ~ lon, lat = ~ lat, color = ~ factpal(type)) %>% 
+#   addTiles()
+# 
+# m %>% addProviderTiles(providers$CartoDB.Positron)
+# 
+# m %>% addTiles()
+# 
+# m <- leaflet() %>%
+#   addTiles() %>%  # Add default OpenStreetMap map tiles
+#   addMarkers(lng=174.768, lat=-36.852, popup="The birthplace of R")
